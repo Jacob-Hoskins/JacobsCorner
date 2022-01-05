484ce2b4dc2810c761fb4bbbbdbbfd2ac3c69401
Jacob Hoskins <jhoskins997@gmail.com>
2022-01-04 18:40:47 -0800

    same

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -43,7 +43,7 @@ function Nav() {
 
      @media (max-width: 500px){
          span{
-             font-size: 9px;
+             font-size: 7px;
              text-align: center;
              justify-content: center;
          }
