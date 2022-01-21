d7d44db20574ae267cb2a2a8fba6bd3ffdcba759
Jacob Hoskins <jhoskins997@gmail.com>
2022-01-21 11:44:45 -0800

    Got rid of Videos, netlify doesnt support

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { BrowserRouter as Router, Routes, Route, Navigate, Link} from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home';
-import Videos from './Pages/Videos';
+// import Videos from './Pages/Videos';
 import Articles from './Pages/Articles';
 import Projects from './Pages/Projects';
 import Social from './Pages/Social';
@@ -20,7 +20,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/videos' element={<Videos />} />
+        {/* <Route path='/videos' element={<Videos />} /> */}
         <Route path='/articles' element={<Articles />} />
         <Route path='/projects' element={<Projects />} />
         <Route path='/social' element={<Social />} />
