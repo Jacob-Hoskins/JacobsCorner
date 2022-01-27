415be8f9d5ad8916892f6a7ae8c82d8c4788c599
Jacob Hoskins <jhoskins997@gmail.com>
2022-01-27 11:36:21 -0800

    Unadded changes for selecting article language type

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,12 +3,14 @@ import styled from 'styled-components'
 import VideoSlider from '../components/VideoSlider'
 import HomeArticles from '../components/HomeArticles'
 import Nav from '../components/Nav'
+import LanguageSelector from '../components/LanguageSelector'
 
 function Home() {
     return (
         <Container>
             <Nav />
             <VideoSlider />
+            {/* <LanguageSelector /> */}
             <HomeArticles />
             
         </Container>
---
+++ src/components/LanguageSelector.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+function LanguageSelector() {
+  return (
+    <Container>
+        <img src="../Pictures/PythonImage.jpg" alt="Python Image" />
+        <img src="../Pictures/JavascriptImage.png" alt="Javascript Image" />
+    </Container>
+  )
+}
+
+export default LanguageSelector;
+
+const Container = styled.div`
+
+`
